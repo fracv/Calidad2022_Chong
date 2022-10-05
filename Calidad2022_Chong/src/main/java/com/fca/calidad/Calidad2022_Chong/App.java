218255481ced2a16731aaf1576d0145eb0f9d224
fracv <A16003579@alumnos.uady.mx>
2022-10-05 13:31:07 -0500

    Adición Maven

---
+++ Calidad2022_Chong/src/main/java/com/fca/calidad/Calidad2022_Chong/App.java
@@ -0,0 +1,13 @@
+package com.fca.calidad.Calidad2022_Chong;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
