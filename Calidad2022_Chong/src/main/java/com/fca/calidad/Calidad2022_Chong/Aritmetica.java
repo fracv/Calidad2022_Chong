c44e02fcbced84d039b6fed7ee819f883b1df7c7
fca <fca@FCA-CC-C06.inet.uady.mx>
2022-10-05 19:06:34 -0500

    Adición de clase Aritmética.

---
+++ Calidad2022_Chong/src/main/java/com/fca/calidad/Calidad2022_Chong/Aritmetica.java
@@ -0,0 +1,20 @@
+package com.fca.calidad.Calidad2022_Chong;
+
+public class Aritmetica {
+	private float ultimoResultado;
+	public float suma(float primerSumando, float segundoSumando) {
+		return ultimoResultado = primerSumando + segundoSumando;
+	}
+	public float resta(float minuendo, float sustraendo) {
+		return ultimoResultado = minuendo - sustraendo;
+	}
+	public float multiplicacion(float primerFactor, float segundoFactor) {
+		return ultimoResultado = primerFactor * segundoFactor;
+	}
+	public float division(float dividendo, float divisor) {
+		return ultimoResultado = dividendo/divisor;
+	}
+	public float getUltimoResultado() {
+		return ultimoResultado;
+	}
+}
