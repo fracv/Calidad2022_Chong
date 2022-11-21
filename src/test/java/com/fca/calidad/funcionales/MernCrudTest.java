af36a22d7f85404519ae44777223f984aafdb32d
fracv <A16003579@alumnos.uady.mx>
2022-11-20 21:09:57 -0600

    Solución de errores

--- src/test/java/com/fca/calidad/funcionales/MernCrudTest.java
+++ src/test/java/com/fca/calidad/funcionales/MernCrudTest.java
@@ -42,8 +42,10 @@ public class MernCrudTest {
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
     pause(2000);
-    String msg = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nice one!'])[1]/following::p[1]")).getAttribute("textContent");
-    assertThat(msg, is("Successfully added!"));
+    driver.findElement(By.xpath("//i")).click();
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Walter White[\\s\\S]*$"));
+    //String msg = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nice one!'])[1]/following::p[1]")).getAttribute("textContent");
+    //assertThat(msg, is("Successfully added!"));
   }
  
   @Test
@@ -72,8 +74,10 @@ public class MernCrudTest {
     driver.findElement(By.name("email")).sendKeys("heisenberg@meta.com");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
     pause(2000);
-    String msg = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nice one!'])[1]/following::p[1]")).getAttribute("textContent");
-    assertThat(msg, is("Successfully updated!"));
+    driver.findElement(By.xpath("//i")).click();
+    assertTrue(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Heisenberg[\\s\\S]*$"));
+    //String msg = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nice one!'])[1]/following::p[1]")).getAttribute("textContent");
+    //assertThat(msg, is("Successfully updated!"));
   }
   
   @Test
@@ -93,10 +97,11 @@ public class MernCrudTest {
     }
     while(x != 1000);
     
-    driver.findElement(By.xpath("//*[@id=\"root\"]/div/div[2]/table/tbody/tr["+i+"]/td[5]/button[2]")).click();
+    driver.findElement(By.xpath("//div[@id='root']/div/div[2]/table/tbody/tr["+i+"]/td[5]/button[2]")).click();
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Heisenberg'])[2]/following::button[1]")).click();
     pause(2000);
-    assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("Heisenberg"));
+    assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("^[\\s\\S]*Heisenberg[\\s\\S]*$"));
+    //assertFalse(driver.findElement(By.cssSelector("BODY")).getText().matches("Heisenberg"));
   }
   
   @Before
