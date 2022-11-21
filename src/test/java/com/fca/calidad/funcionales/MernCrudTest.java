2bf24673e9ace90bf8a5096137312b420c6f344d
fracv <A16003579@alumnos.uady.mx>
2022-11-20 20:01:46 -0600

    Solución de errores

--- src/test/java/com/fca/calidad/funcionales/MernCrudTest.java
+++ src/test/java/com/fca/calidad/funcionales/MernCrudTest.java
@@ -42,7 +42,7 @@ public class MernCrudTest {
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Male'])[1]/following::div[2]")).click();
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
     pause(2000);
-    String msg = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getAttribute("textContent");
+    String msg = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nice one!'])[1]/following::p[1]")).getAttribute("textContent");
     assertThat(msg, is("Successfully added!"));
   }
  
@@ -72,7 +72,7 @@ public class MernCrudTest {
     driver.findElement(By.name("email")).sendKeys("heisenberg@meta.com");
     driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Woah!'])[1]/following::button[1]")).click();
     pause(2000);
-    String msg = driver.findElement(By.xpath("/html/body/div[2]/div/div[2]/form/div[4]/div/p")).getAttribute("textContent");
+    String msg = driver.findElement(By.xpath("(.//*[normalize-space(text()) and normalize-space(.)='Nice one!'])[1]/following::p[1]")).getAttribute("textContent");
     assertThat(msg, is("Successfully updated!"));
   }
   
